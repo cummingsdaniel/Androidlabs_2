df93904dc68154640c78356fe74403f64431023c
cummi <cummingsdan87@gmail.com>
2019-09-24 11:10:59 -0400

    this is lab 3 Starting step 6

--- app/src/main/java/com/example/androidlabs_2/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs_2/ProfileActivity.java
@@ -3,12 +3,14 @@ package com.example.androidlabs_2;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
@@ -19,6 +21,13 @@ public class ProfileActivity extends AppCompatActivity {
         String emailBox = savedEmail.getString("ReserveEmail", "no Value");
         inputEmail.setText(emailBox);
         Log.d("ProfileActivity", "Email="+emailBox);
-
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
 }
