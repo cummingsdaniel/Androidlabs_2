a8680f812ac394835a065044fa6205c7bf096d21
danstonmurphy <cumm0082@algonquinlive.com>
2019-09-25 11:11:43 -0400

    Fix Lab2

--- app/src/main/java/com/example/androidlabs_2/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs_2/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.androidlabs_2;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -11,15 +12,21 @@ import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+    private ImageButton mImageButton;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         EditText inputName = findViewById(R.id.editTextName);
         EditText inputEmail = findViewById(R.id.emailEditText);
-        ImageButton picButton = findViewById(R.id.pic_button);
         SharedPreferences savedEmail = getSharedPreferences("Users", MODE_PRIVATE);
         String emailBox = savedEmail.getString("ReserveEmail", "no Value");
         inputEmail.setText(emailBox);
+        ImageButton mImageButton = findViewById(R.id.pic_button);
+        mImageButton.setOnClickListener(clk -> ){
+            startActivityForResult();
+        };
         Log.d("ProfileActivity", "Email="+emailBox);
     }
 
@@ -28,6 +35,41 @@ public class ProfileActivity extends AppCompatActivity {
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+            Log.e(ACTIVITY_NAME, "in function: ");
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "in function: ");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "in function: ");
+
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "in function: ");
+    }
+
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "in function: ");
     }
 
 }
