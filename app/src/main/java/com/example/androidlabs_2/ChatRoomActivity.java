31cd1bc2e6a86746ca57752b85e93c3f8e0d02af
cummi <cummingsdan87@gmail.com>
2019-09-28 14:54:04 -0400

    This is lab 4 new activity

--- app/src/main/java/com/example/androidlabs_2/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs_2/ChatRoomActivity.java
@@ -1,11 +1,16 @@
 package com.example.androidlabs_2;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,19 +18,40 @@ import java.util.ArrayList;
 
 public class ChatRoomActivity extends AppCompatActivity {
     ArrayList<Message> chat;
+    BaseAdapter theAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
         chat = new ArrayList<>();
+        EditText mess = (EditText) findViewById(R.id.messageInput);
         ListView listings = findViewById(R.id.theList);
-        listings.setAdapter();
+        listings.setAdapter(theAdapter = new MyListAdapter());
+        Button sendButton = findViewById(R.id.sendButton);
+        Button receiveButton = findViewById(R.id.receiveButton);
+
+        sendButton.setOnClickListener(v -> {
+            Log.d("sendButtonClicked", "bbbb");
+            Message mleft = new Message(mess.getText().toString(), true);
+            chat.add(mleft);
+            theAdapter.notifyDataSetChanged();
+            mess.setText("");
+        });
+
+        receiveButton.setOnClickListener(v -> {
+            Log.d("RecButtonClicked", "bbbb");
+            Message mright = new Message(mess.getText().toString(), false);
+            chat.add(mright);
+            theAdapter.notifyDataSetChanged();
+            mess.setText("");
+        });
+
     }
     private class MyListAdapter extends BaseAdapter {
 
         public int getCount() {  return chat.size();  } //This function tells how many objects to show
 
-        public String getItem(int position) { return chat.get(position);  }  //This returns the string at position p
+        public Message getItem(int position) { return chat.get(position);  }  //This returns the string at position p
 
         public long getItemId(int p) { return p; } //This returns the database id of the item at position p
 
@@ -33,20 +59,37 @@ public class ChatRoomActivity extends AppCompatActivity {
         {
 
             LayoutInflater inflater = getLayoutInflater();
-            View thisRow = recycled;
+            View thisRow = null;
 
-            if(thisRow == null) {
-                thisRow = inflater.inflate(R.layout.table_row_layout, null);
-            }
+            Log.d("fdfggdg", chat.get(p).toString());
 
+
+                if(chat.get(p).isSent()) {
+                    thisRow = inflater.inflate(R.layout.table_row_left, null);
+                }
+                else if(!chat.get(p).isSent()){
+                    thisRow = inflater.inflate(R.layout.table_row_right, null);
+                }
+
+Log.d("gggggggg",getItem(p).getChat());
             TextView itemField = thisRow.findViewById(R.id.itemField);
-            itemField.setText(getItem(p));
+            itemField.setText(getItem(p).getChat());
 
-            TextView numberField = thisRow.findViewById(R.id.numberField);
-            numberField.setText(Integer.toString(p));
+
 
             return thisRow;
         }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
-}
+
+
--- app/src/main/java/com/example/androidlabs_2/Message.java
+++ app/src/main/java/com/example/androidlabs_2/Message.java
@@ -2,20 +2,36 @@ package com.example.androidlabs_2;
 
 public class Message {
     private String chat;
+    private boolean sent;
     public Message() {
 
     }
-    public Message(String s) {
+    public Message(String s, boolean sent) {
         this.chat=s;
+        this.sent=sent;
     }
 
     public void setChat(String chat) {
         this.chat = chat;
     }
 
-
-
     public String getChat() {
         return chat;
+    }
+
+    public boolean isSent() {
+        return sent;
+    }
+
+    public void setSent(boolean sent) {
+        this.sent = sent;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "chat='" + chat + '\'' +
+                ", sent=" + sent +
+                '}';
     }
 }
