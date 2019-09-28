410e8ddbb7b9a722bb43d7b8d11b8a971dc6190d
cummi <cummingsdan87@gmail.com>
2019-09-28 13:32:09 -0400

    This is lab 4 new activity

--- app/src/main/java/com/example/androidlabs_2/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs_2/ChatRoomActivity.java
@@ -2,12 +2,51 @@ package com.example.androidlabs_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ChatRoomActivity extends AppCompatActivity {
-
+    ArrayList<Message> chat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
+        chat = new ArrayList<>();
+        ListView listings = findViewById(R.id.theList);
+        listings.setAdapter();
     }
+    private class MyListAdapter extends BaseAdapter {
+
+        public int getCount() {  return chat.size();  } //This function tells how many objects to show
+
+        public String getItem(int position) { return chat.get(position);  }  //This returns the string at position p
+
+        public long getItemId(int p) { return p; } //This returns the database id of the item at position p
+
+        public View getView(int p, View recycled, ViewGroup parent)
+        {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View thisRow = recycled;
+
+            if(thisRow == null) {
+                thisRow = inflater.inflate(R.layout.table_row_layout, null);
+            }
+
+            TextView itemField = thisRow.findViewById(R.id.itemField);
+            itemField.setText(getItem(p));
+
+            TextView numberField = thisRow.findViewById(R.id.numberField);
+            numberField.setText(Integer.toString(p));
+
+            return thisRow;
+        }
+    }
+}
 }
---
+++ app/src/main/java/com/example/androidlabs_2/Message.java
@@ -0,0 +1,21 @@
+package com.example.androidlabs_2;
+
+public class Message {
+    private String chat;
+    public Message() {
+
+    }
+    public Message(String s) {
+        this.chat=s;
+    }
+
+    public void setChat(String chat) {
+        this.chat = chat;
+    }
+
+
+
+    public String getChat() {
+        return chat;
+    }
+}
