2edaccd9201760f156f3c0a79b404ff41ed66446
cummi <cummingsdan87@gmail.com>
2019-09-28 13:02:23 -0400

    This is lab 4 new activity

--- app/src/main/java/com/example/androidlabs_2/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs_2/ChatRoomActivity.java
@@ -1,4 +1,13 @@
 package com.example.androidlabs_2;
 
-public class ChatRoomActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+    }
 }
