7465ffabec79ae09101129e0f3f7bb10d855811c
cummi <cummingsdan87@gmail.com>
2019-11-08 14:00:15 -0500

    Lab 6 step4

--- app/src/main/java/com/example/androidlabs_2/WeatherForecast.java
+++ app/src/main/java/com/example/androidlabs_2/WeatherForecast.java
@@ -2,6 +2,7 @@ package com.example.androidlabs_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class WeatherForecast extends AppCompatActivity {
 
@@ -9,6 +10,6 @@ public class WeatherForecast extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast);
-
+        ImageView weatherImageView = new ImageView();
     }
 }
