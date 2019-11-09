000969a2f4b96fd538808197733ba08bc44d2447
cummi <cummingsdan87@gmail.com>
2019-11-09 10:29:43 -0500

    Lab 6 step6

--- app/src/main/java/com/example/androidlabs_2/WeatherForecast.java
+++ app/src/main/java/com/example/androidlabs_2/WeatherForecast.java
@@ -1,5 +1,6 @@
 package com.example.androidlabs_2;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,8 +9,26 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class WeatherForecast extends AppCompatActivity {
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.xmlpull.v1.XmlPullParser.END_TAG;
+import static org.xmlpull.v1.XmlPullParser.START_TAG;
+import static org.xmlpull.v1.XmlPullParser.TEXT;
+
+public class WeatherForecast extends AppCompatActivity {
+    public String wind;
+    public String UV;
+    public String minTemperature;
+    public String maxTemperature;
+    public Bitmap picOfCurrentWeather;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +40,54 @@ public class WeatherForecast extends AppCompatActivity {
         TextView uvRatingTextView = findViewById(R.id.uvRatingTextView);
         ProgressBar progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(View.VISIBLE);
+
+        ForecastQuery startForecast = new ForecastQuery();
+        startForecast.execute();
     }
     private class ForecastQuery extends AsyncTask<String, Integer, String> {
+    String responseType;
 
-}
+        @Override
+        protected String doInBackground(String... strings) {
+            String ret = null;
+            String queryURL = "http://torunski.ca/CST2335_XML.xml";
+
+            try {       // Connect to the server:
+                URL url = new URL(queryURL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inStream = urlConnection.getInputStream();
+
+                //Set up the XML parser:
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput( inStream  , "UTF-8");
+
+                //Iterate over the XML tags:
+                int EVENT_TYPE;         //While not the end of the document:
+                while((EVENT_TYPE = xpp.getEventType()) != XmlPullParser.END_DOCUMENT)
+                {
+                    switch(EVENT_TYPE)
+                    {
+                        case START_TAG:         //This is a start tag < ... >
+                            String tagName = xpp.getName(); // What kind of tag?
+                            if(tagName.equals(""))
+                            {
+                            }
+                            break;
+                        case END_TAG:           //This is an end tag: </ ... >
+                            break;
+                        case TEXT:              //This is text between tags < ... > Hello world </ ... >
+                            break;
+                    }
+                    xpp.next(); // move the pointer to next XML element
+                }
+            }
+            catch(MalformedURLException mfe){ ret = "Malformed URL exception"; }
+            catch(IOException ioe)          { ret = "IO Exception. Is the Wifi connected?";}
+            catch(XmlPullParserException pe){ ret = "XML Pull exception. The XML is not properly formed" ;}
+            //What is returned here will be passed as a parameter to onPostExecute:
+            return ret;
+        }
+    }
 }
