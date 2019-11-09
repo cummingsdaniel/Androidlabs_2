75745df042ba0058b75addf276df5daa4a20cd50
cummi <cummingsdan87@gmail.com>
2019-11-09 09:30:43 -0500

    Lab 6 step5

--- app/src/main/java/com/example/androidlabs_2/WeatherForecast.java
+++ app/src/main/java/com/example/androidlabs_2/WeatherForecast.java
@@ -1,8 +1,12 @@
 package com.example.androidlabs_2;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class WeatherForecast extends AppCompatActivity {
 
@@ -10,6 +14,15 @@ public class WeatherForecast extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast);
-        ImageView weatherImageView = new ImageView();
+        ImageView weatherImageView = findViewById(R.id.weather_imageView);
+        TextView currTempTextView = findViewById(R.id.currentTempTextView);
+        TextView minTempTextView = findViewById(R.id.minTempTextView);
+        TextView maxTempTextView = findViewById(R.id.maxTempTextView);
+        TextView uvRatingTextView = findViewById(R.id.uvRatingTextView);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
     }
+    private class ForecastQuery extends AsyncTask<String, Integer, String> {
+
+}
 }
