f0a138dd3ab09fe4ae015419407ad5cea564bc75
cummi <cummingsdan87@gmail.com>
2019-11-12 12:58:43 -0500

    Lab 7 step 4

--- app/src/main/java/com/example/androidlabs_2/TestToolBar.java
+++ app/src/main/java/com/example/androidlabs_2/TestToolBar.java
@@ -2,6 +2,7 @@ package com.example.androidlabs_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class TestToolBar extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class TestToolBar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_tool_bar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setActionBar(myToolbar);
     }
 }
