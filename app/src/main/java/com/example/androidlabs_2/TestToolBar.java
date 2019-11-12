cf076e3adf76fc17a708c27cb33ed3c7ba982187
cummi <cummingsdan87@gmail.com>
2019-11-12 14:58:28 -0500

    Lab 7 step 6

--- app/src/main/java/com/example/androidlabs_2/TestToolBar.java
+++ app/src/main/java/com/example/androidlabs_2/TestToolBar.java
@@ -2,6 +2,10 @@ package com.example.androidlabs_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class TestToolBar extends AppCompatActivity {
@@ -12,5 +16,25 @@ public class TestToolBar extends AppCompatActivity {
         setContentView(R.layout.activity_test_tool_bar);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "You clicked on the overflow menu", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
