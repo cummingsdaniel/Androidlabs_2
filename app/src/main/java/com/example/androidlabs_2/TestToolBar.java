6caca38c1eea4120b2fbaf85888a8f1f1ee0d469
cummi <cummingsdan87@gmail.com>
2019-11-16 11:55:48 -0500

    Lab7.2 just toolbar works

--- app/src/main/java/com/example/androidlabs_2/TestToolBar.java
+++ app/src/main/java/com/example/androidlabs_2/TestToolBar.java
@@ -49,4 +49,29 @@ public class TestToolBar extends AppCompatActivity {
         }
         return true;
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
