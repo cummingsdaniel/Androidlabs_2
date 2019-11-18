00d585092d636368e8928eb17ed09115efd6f7ca
cummi <cummingsdan87@gmail.com>
2019-11-18 11:12:54 -0500

    Lab 7 step 9

--- app/src/main/java/com/example/androidlabs_2/TestToolBar.java
+++ app/src/main/java/com/example/androidlabs_2/TestToolBar.java
@@ -18,7 +18,7 @@ import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 public class TestToolBar extends AppCompatActivity {
-
+    public String newMessage = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,24 +32,48 @@ public class TestToolBar extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
-
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.item1:
-                Toast.makeText(this, "You clicked on the overflow menu", Toast.LENGTH_SHORT)
-                        .show();
+                Toast.makeText(this, "This is the initial message", Toast.LENGTH_SHORT).show();
                 break;
-
-                default:
+            case R.id.item2:
+                alertBox();
                 break;
-        }
-        return true;
+            case R.id.item3:
+                if(newMessage.equals("")) {
+                    newMessage = "This is the initial message";
+                }
+                Toast.makeText(this, newMessage, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }return true;
     }
+    public void alertBox() {
+        View middle = getLayoutInflater().inflate(R.layout.dialog_box, null);
+        EditText input = (EditText)middle.findViewById(R.id.view_edit_text);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("What is your message")
+                .setPositiveButton("+", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        newMessage = input.getText().toString();
+                    }
+                })
+                .setNegativeButton("-", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setView(middle);
+        builder.create().show();
+    }
     @Override
     protected void onPause() {
         super.onPause();
